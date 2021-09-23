d129f5e79a8310a16d92dd23636c955ba34122df
zzangbomi <53842993+zzangbomi@users.noreply.github.com>
2021-09-23 14:21:40 +0900

    Add files via upload

---
+++ 5주차/이보미/Main_9372.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main_9372 {
+
+	static int T, N, M;
+	
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		T = scann.nextInt();
+		
+		for (int t = 0; t < T; t++) {
+			N = scann.nextInt();
+			M = scann.nextInt();
+			
+			for (int i = 0; i < M; i++) {
+				int start = scann.nextInt();
+				int end = scann.nextInt();
+			}
+			
+			System.out.println(N-1);
+		}
+	}
+}
