8afcfa41b03a1c2d686c82bed07c33932a63400d
JeongWon Been <congcoding@gmail.com>
2021-10-19 23:51:41 +0900

    N과 M (6) - 조합

---
+++ 9주차/빈정원/Main_BOJ_15655.java
@@ -0,0 +1,42 @@
+package _09_1020;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// N과 M (6)
+public class Main_BOJ_15655 {
+
+	static int N, M;
+	static int[] arr;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		
+		combination(0, 0, new int[M]);
+		System.out.println(sb.toString());
+	}
+
+	private static void combination(int start, int cnt, int[] nums) {
+		if (cnt == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(arr[nums[i]] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = start; i < N; i++) {
+			nums[cnt] = i;
+			combination(i + 1, cnt + 1, nums);
+		}
+	}
+
+}
