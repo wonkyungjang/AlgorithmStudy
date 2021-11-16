7dc831da4df5fe54d0ffc7f64cd9b871c8122d57
zzangbomi <53842993+zzangbomi@users.noreply.github.com>
2021-11-16 10:26:39 +0900

    포도주 시식

--- 13주차/이보미/Main_2156.java
+++ 13주차/이보미/Main_2156.java
@@ -20,24 +20,24 @@ public class Main_2156 {
 			wine[i] = Integer.parseInt(br.readLine());
 		}
 		
-	// 첫잔일 경우
-        if (n >= 1) {
-            dp[0] = wine[0];
-        } 
-        // 두잔일 경우
-        if (n >= 2) {
-            dp[1] = wine[0] + wine[1];
-        }
-        // 세잔일 경우
-        if (n >= 3) {
-            dp[2] = Math.max(dp[1], Math.max(dp[0] + wine[2], wine[1] + wine[2]));
-        }
-        for (int i = 3; i < n; i++) {
-        	// oox, oxo, xoo 중 가장 큰 값을 구하면 됨
-            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i]));
-        }
+		// 첫잔일 경우
+		if (n >= 1) {
+		    dp[0] = wine[0];
+		} 
+		// 두잔일 경우
+		if (n >= 2) {
+		    dp[1] = wine[0] + wine[1];
+		}
+		// 세잔일 경우
+		if (n >= 3) {
+		    dp[2] = Math.max(dp[1], Math.max(dp[0] + wine[2], wine[1] + wine[2]));
+		}
+		for (int i = 3; i < n; i++) {
+			// oox, oxo, xoo 중 가장 큰 값을 구하면 됨
+		    dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i]));
+		}
         
-        System.out.println(dp[n - 1]);
+        	System.out.println(dp[n - 1]);
 	}
 
 }
